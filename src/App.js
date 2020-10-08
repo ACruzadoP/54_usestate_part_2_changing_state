1c30fbf6db855b18d7a6e0e1b99c017fa1ef77dc
Alberto <albertocruzadopleite@gmail.com>
2020-10-08 05:00:24 +0200

    Tomando snapshot de un ejercicio terminado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//NEW WAY
+function App () {
+    const [ count, setCount ] = React.useState(0)
+    const [ answer, setAnswer] = React.useState("Yes")
+
+    function increment () {
+        setCount(prevCount => prevCount + 1)
+    }
+
+    function decrement () {
+        setCount(prevCount => prevCount - 1)
+    }
+
+    function change () {
+        answer === "Yes" ?
+        setAnswer("No") :
+        setAnswer("Yes")
+    }
+
+    return(
+        <div>
+            <h1>{count}</h1>
+            <button onClick={increment}>Increment!</button>
+            <button onClick={decrement}>Decrement!</button>
+            <h1>{answer}</h1>
+            <button onClick={change}>Change!</button>
+        </div>
+    )
 }
 
-export default App;
+//OLD WAY
+/*
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>{this.state.count}</h1>
+                <button>Change!</button>
+            </div>
+        )
+    }
+}
+*/
+
+export default App
+
